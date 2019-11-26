75ab63ad7edf8771adfa7184a61393ec7f1a4b7a
Vikas Raj Yadav <vikasyadav.vision111@gmail.com>
2019-11-26 20:14:20 +0530

    Pic added

--- src/components/header.js
+++ src/components/header.js
@@ -5,12 +5,12 @@ import './Header.css'
 const Header = ({ siteTitle }) => (
   <div className="Header">
     <div className="HeaderGroup">
-    <Link to ="https://www.linkedin.com/in/vraj247/">Linkedin</Link>
-    <Link to ="https://www.behance.net/vraj247">Behance</Link>
-    <Link to ="https://dribbble.com/Vraj247">Dribbble</Link>
-    <Link to ="https://www.instagram.com/vraj247/">Instagram</Link>
-    <Link to ="https://github.com/Vraj247">Github</Link>
-    </div>
+    <a href="https://www.linkedin.com/in/vraj247/" target="_blank">Linkedin</a>
+    <a href="https://www.behance.net/vraj247" target="_blank">Behance</a>
+    <a href="https://dribbble.com/Vraj247" target="_blank">Dribbble</a>
+    <a href="https://www.instagram.com/vraj247/" target="_blank">Instagram</a>
+    <a href="https://github.com/Vraj247/Vraj247" target="_blank">Github</a>
+  </div>
   </div>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,12 @@ const IndexPage = () => (
   <div>
     <div className="Hero">
       <div className="HeroGroup">
-      <h1>👋 Hello Human! </h1>
+      <div className="img">
+      <img src = "https://avatars3.githubusercontent.com/u/14306259?s=460&v=4"></img>
+      </div>  
+      <h1>👋 Hello humans! </h1>
       <p>I'm a Product Designer, currenly designing digital products for 100 Million+ humans at Paytm. I believe in designing experiences which solves problems but looks cool at the same time. Found myself between a designer and an Engineer </p>
-      <Link to= "/Page2/">Don't be shy, Say hi!</Link> 
+      <Link to= "/Page2/">In Progress</Link> 
       </div>
     </div>
   </div>

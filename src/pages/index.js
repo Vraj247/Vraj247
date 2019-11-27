7a20976be225ad69ca1f3bd7e76fe028df372683
Vikas Raj Yadav <vikasyadav.vision111@gmail.com>
2019-11-27 13:57:49 +0530

    button change

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,12 @@ const IndexPage = () => (
   <div>
     <div className="Hero">
       <div className="HeroGroup">
-      <div className="img">
-      <img src="https://p21.f4.n0.cdn.getcloudapp.com/items/5zu54DdJ/mypic.png?v=0cb29474ad8ffb955c79f8f14c34c94b" alt="Cinque Terre" width="400" height="400"></img>
-      </div>  
+        <div className="img">
+        <img src="https://p21.f4.n0.cdn.getcloudapp.com/items/5zu54DdJ/mypic.png?v=0cb29474ad8ffb955c79f8f14c34c94b" alt="Cinque Terre" width="400" height="400"></img>
+        </div>  
       <h1>Welcome fellow Human! </h1>
-      <p>Myself Vikas Raj Yadav, a Product Designer currenly in Bangalore,India. I'm a self taught designer who  </p>
-      <Link to= "/Page2/">In Progress</Link> 
+      <p>I'm Vikas Raj Yadav, a Product Designer currenly in Bangalore,India. Designing digital experiences for humans at Paytm. I code sometimes. A Designer by profession, Engineer by education. </p>
+      <a className ="cta">Wanna talk Design?</a>
       </div>
     </div>
   </div>

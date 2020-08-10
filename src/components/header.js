3930ff0e3705a28560194f242c754bcd230cf144
Vikas Raj Yadav <vikasyadav.vision111@gmail.com>
2020-08-10 16:40:18 +0530

    Changes added Remove the links

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,40 @@
 import React from 'react'
-import Link from 'gatsby-link'
 import './Header.css'
 
 const Header = ({ siteTitle }) => (
   <div className="Header">
-    <div className="HeaderGroup">
-    <a href="https://www.linkedin.com/in/vraj247/" target="_blank">Linkedin</a>
-    <a href="https://www.behance.net/vraj247" target="_blank">Behance</a>
-    <a href="https://dribbble.com/Vraj247" target="_blank">Dribbble</a>
-    <a href="https://www.instagram.com/vraj247/" target="_blank">Instagram</a>
-    <a href="https://github.com/Vraj247/Vraj247" target="_blank">Github</a>
-  </div>
+    <nav className="topnav">
+      <a
+        href="https://www.linkedin.com/in/vraj247/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Linkedin
+      </a>
+      <a
+        href="https://www.behance.net/vraj247"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Behance
+      </a>
+      <a
+        href="https://dribbble.com/Vraj247"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src = "https://vijayverma.co/dribbble.svg">
+        Dribbble
+      </a>
+
+      <a
+        href="https://github.com/Vraj247/Vraj247"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Settings
+      </a>
+    </nav>
   </div>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,15 @@
 import React from 'react'
-import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
+    <div classname="heroimg"></div>
+
     <div className="Hero">
       <div className="HeroGroup">
-        <div className="img">
-        <img src="https://p21.f4.n0.cdn.getcloudapp.com/items/5zu54DdJ/mypic.png?v=0cb29474ad8ffb955c79f8f14c34c94b" alt="Cinque Terre" width="400" height="400"></img>
-        </div>  
-      <h1>Hi, I'm Vikas Raj Yadav.</h1>
-      <div className="Subtext">
-        <p>A Product UX Designer at Paytm workin on digital experiences for 100 million+ humans. Before joining Paytm, I worked at Grappus Studios where I worked on some of the most interesting projects like Uber DTI, Scio, PVR cinemas, freadom, Starquik etc.</p>
-      </div>
-      <div className= "button">
-        <a href="mailto:vikasyadav.vision111@gmail.com?Subject=Hey%20Vikas" target="_top">Say Hi!</a>
-      </div>
+        <h1>I'm a product designer based in Banglore, India.</h1>
+        <div className="Subtext">
+          <p>Currently designing motion experiences at Paytm.</p>
+        </div>
       </div>
     </div>
   </div>
